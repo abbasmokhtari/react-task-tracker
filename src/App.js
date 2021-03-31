febb47cee88fd4d3782579fd4bdf92073794fec4
Abbas Mokhtari <49838027+abbasmokhtari@users.noreply.github.com>
2021-03-31 16:54:35 +0100

    json server added and db.json was created in correct format which is keys and strings are in ""

--- src/App.js
+++ src/App.js
@@ -7,26 +7,7 @@ import AddTask from './Components/AddTask'
 function App() {
   const [showAddTask, setShowAddTask] = useState(false)
   // above is defined to toggle the list on screen
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: 'Doctors appointment',
-      day: 'Feb 5th at 2:30pm',
-      reminder: true,
-    },
-    {
-      id: 2,
-      text: 'Meeting at school',
-      day: 'Feb 6th at 1:30pm',
-      reminder: true,
-    },
-    {
-      id: 3,
-      text: 'Food Shopping',
-      day: 'Feb 5th at 2:30pm',
-      reminder: false,
-    },
-  ])
+  const [tasks, setTasks] = useState([])
 
   // Add Task
   const addTask = (task) => {
